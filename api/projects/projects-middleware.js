6eaa863db88d4ed51f8b92a9ad6941d6dbf6a0fb
Shane A. Healy <ShaneAHealy55@gmail.com>
2024-08-21 21:42:58 -0500

    Saved Changes

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,30 @@
-// add middlewares here related to projects
+const Project = require('./projects-model');
+
+const validate_Project_ID = async (req, res, next) => {
+  const project_ID = await Project.get(req.params.id);
+  if (!project_ID) {
+    res.status(404).json({
+      message: "id not found",
+    });
+  } else {
+    req.project_ID = project_ID;
+    next();
+  }
+}
+
+const validate_Project = (req, res, next) => {
+  const { name, description, completed } = req.body;
+  if (typeof name !== 'string' || typeof description !== 'string' || typeof completed !== 'boolean' || name.trim() === '' || description.trim() === '') {
+    res.status(400).json({
+      message: 'name and description required'
+    })
+  } else {
+    req.name = name.trim();
+    req.description = description.trim();
+    req.completed = completed 
+
+    next();
+  }
+}
+
+module.exports = { validate_Project_ID, validate_Project};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,63 @@
-// Write your "projects" router here!
+const express = require('express');
+const Project = require('./projects-model');
+const Action = require('../actions/actions-model');
+const { validate_Project_ID, validate_Project } = require('./projects-middleware'); 
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  Project.get()
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(err => {
+      res.json('[]');
+    })
+  })
+
+router.get('/:id', validate_Project_ID, (req, res, next) => {
+  const id = req.param.id;
+  Project.get(id)
+    .then(project => {
+      res.json(req.project_ID)
+    })
+    .catch(next)
+})
+
+router.post('/', validate_Project, (req, res, next) => {
+  Project.insert({ name: req.name, description: req.description, completed: req.completed })
+    .then(new_project => {
+      res.json(new_project);
+      console.log(new_project)
+    })
+    .catch(next)
+})
+
+router.put('/:id', validate_Project_ID, validate_Project, (req, res, next) => {
+  Project.update(req.params.id, { name: req.name, description: req.description, completed: req.completed })
+    .then(updated_project => {
+      res.json(updated_project);
+    })
+    .catch(next)
+})
+
+router.delete('/:id', validate_Project_ID, async (req, res, next) => {
+  try {
+    await Project.remove(req.params.id);
+    const projects = await Project.get();
+    res.status(200).json(projects)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id/actions', validate_Project_ID, (req, res, next) => {
+  Project.getProjectActions(req.params.id)
+    .then(actions => {
+      res.json(actions);
+    })
+    .catch(next)
+})
+
+module.exports = router;
+
